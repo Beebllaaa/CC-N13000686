2f1fcb94ff53f07490c8d4fa427457ffbb95d773
Beebllaaa <xc3447@nyu.edu>
2026-09-18 02:17:10 +0800

    introduce xPods and yPods to define the situation

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,11 +1,16 @@
+let xPods;
+let yPods;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     colorMode(HSB);
     background(180,10,90);
+    xPods = width/2;
+    yPods = height/2;
 }
 
 function draw() {
-    translate(width/2, height/2);
+    translate(xPods, yPods);
+    fill(120,40,60);   
     noStroke();
     ellipse(0,0,50);
 }
