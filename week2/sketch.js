610dd0f0c0095710ea36a39ebb70370ac55677ea
Beebllaaa <xc3447@nyu.edu>
2026-09-18 02:26:42 +0800

    create the other shapes

--- week2/sketch.js
+++ week2/sketch.js
@@ -3,6 +3,7 @@ let yPods;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    rectMode(CENTER);
     colorMode(HSB);
     background(180,10,90);
     xPods = width/2;
@@ -12,14 +13,42 @@ function setup() {
 }
 
 function draw() {
+    noStroke();
+
     push();
     translate(xPods, yPods);
     rotate(angle);
     fill(120,40,60);   
-    noStroke();
     rect(0,0,50);
     pop();
 
+    push();
+    translate(xPods/2, yPods/2);
+    rotate(angle);
+    fill(120,40,60);   
+    rect(0,0,50);
+    pop();
+
+    push();
+    translate(xPods/2*3, yPods/2);
+    rotate(angle);
+    fill(120,40,60);   
+    rect(0,0,50);
+    pop();
+
+    push();
+    translate(xPods/2, yPods/2*3);
+    rotate(angle);
+    fill(120,40,60);   
+    rect(0,0,50);
+    pop();
+
+    push();
+    translate(xPods/2*3, yPods/2*3);
+    rotate(angle);
+    fill(120,40,60);   
+    rect(0,0,50);
+    pop();
 }
 
 
