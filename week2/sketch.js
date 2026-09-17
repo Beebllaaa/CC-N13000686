6445d6a5bff671854c3cecf278499d038d51fe01
Beebllaaa <xc3447@nyu.edu>
2026-09-18 02:18:25 +0800

    change the circle into rectangle and rotate it

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,16 +1,25 @@
 let xPods;
 let yPods;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     colorMode(HSB);
     background(180,10,90);
     xPods = width/2;
     yPods = height/2;
+    angleMode(DEGREES);
+    angle = random(360);
 }
 
 function draw() {
+    push();
     translate(xPods, yPods);
+    rotate(angle);
     fill(120,40,60);   
     noStroke();
-    ellipse(0,0,50);
+    rect(0,0,50);
+    pop();
+
 }
+
+
