7e65f8c4dd1424e99ba0ee63cd4cda0811688920
Beebllaaa <xc3447@nyu.edu>
2026-09-22 08:20:37 +0800

    make the rectangle move back and forth across the screen

--- week3/sketch.js
+++ week3/sketch.js
@@ -1,11 +1,13 @@
 let recXpods;
 let recYpods;
+let xDir;
 function setup() {
     colorMode(HSB);
     createCanvas(windowWidth, windowHeight);
     background(340,200,100);
     recXpods=windowWidth/2;
     recYpods=windowHeight/2;
+    xDir=1;
 }
 
 function draw() {
@@ -18,4 +20,10 @@ function draw() {
     stroke(12,49,120);
     rect(recXpods, recYpods, 100);
 
+    //make the rectangle move back and forth across the screen
+     recXpods+=xDir*3;
+    if (recXpods>windowWidth-50 || recXpods<50){
+        xDir*=-1;
+    }
+    
 }
