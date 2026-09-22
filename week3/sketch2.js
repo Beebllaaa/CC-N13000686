f0d3884f4433dce81f8cc8e666252d9df0a1062e
Beebllaaa <xc3447@nyu.edu>
2026-09-22 10:36:28 +0800

    change some details

--- week3/sketch2.js
+++ week3/sketch2.js
@@ -5,22 +5,20 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
   background(340,90,100);
-  x=120;
-  y=120;
 }
 
 function draw() {
   rectMode(CENTER);
-  for(x=120; x<windowWidth; x+=300){
-    for(y=120; y<windowHeight; y+=300){
+  for(x=40; x<windowWidth; x+=70){
+    for(y=35; y<windowHeight; y+=70){
       push();
       fill(50,50,100);
       //let strokeweight change with x and y 
-      strokeWeight(40*(x*y/3000000));
+      strokeWeight(40*(x*y/1000000));
       stroke(199,49,120);
       translate(x, y);
       rotate(rot);
-      rect(0, 0, 200);
+      rect(0, 0, 50);
       pop();
       rot+=0.01;
     }
